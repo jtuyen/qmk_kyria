96d7390f4d1bb4c03a82783d700ed1d14ba14ac2
John Tuyen <john.tuyen@gmail.com>
2022-06-05 19:27:49 -0400

    Update config.h

--- config.h
+++ config.h
@@ -17,11 +17,14 @@
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_KNIGHT
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_HUE_STEP  8
 #    define RGBLIGHT_SAT_STEP  8
 #    define RGBLIGHT_VAL_STEP  8
 #    define RGBLIGHT_LIMIT_VAL 150
+#    define RGBLIGHT_SLEEP
 #endif
 
 // Lets you roll mod-tap keys
