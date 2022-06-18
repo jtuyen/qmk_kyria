dba67644c10296075d3bea36aadbeb23aaab43b9
John Tuyen <john.tuyen@gmail.com>
2022-06-18 11:40:03 -0400

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -26,12 +26,14 @@ const uint16_t PROGMEM az_combo[] = {KC_A, KC_Z, COMBO_END};
 const uint16_t PROGMEM sx_combo[] = {KC_S, KC_X, COMBO_END};
 const uint16_t PROGMEM dc_combo[] = {KC_D, KC_C, COMBO_END};
 const uint16_t PROGMEM fv_combo[] = {KC_F, KC_V, COMBO_END};
+const uint16_t PROGMEM fr_combo[] = {KC_F, KC_R, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   [AZ_UNDO] = COMBO(az_combo, LGUI(KC_Z)),
   [SX_ALFRED_SNIPPET] = COMBO(sx_combo, LSG(KC_X)),
   [DC_COPY] = COMBO(dc_combo, LGUI(KC_C)),
   [FV_ALFRED_PASTE] = COMBO(fv_combo, LSG(KC_V)),
+  [FR_PASTE] = COMBO(fr_combo, LGUI(KC_V)),
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[HACK] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, ALL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, TD(TD_LCTL_SPC), TD(TD_LPRN_LBRC), TD(TD_RPRN_RBRC), KC_MINS, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_MUTE, KC_LGUI, KC_LALT, KC_ENT, MO(2), MO(1), KC_SPC, OSM(MOD_RCTL), RCTL(KC_B), KC_MPLY),
