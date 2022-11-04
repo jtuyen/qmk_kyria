9f3d88324f431ca4c362967903f9f4c6eea108ed
John Tuyen <john.tuyen@gmail.com>
2022-11-04 17:00:48 -0400

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -164,9 +164,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     } else if (index == 1) {
         // Page up/Page down
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_LEFT);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_RGHT);
         }
     }
     return false;
