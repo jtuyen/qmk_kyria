a7b59918228f2df685b80338591daf22f21586c4
John Tuyen <john.tuyen@gmail.com>
2022-11-21 21:32:51 -0500

    switching vertical scroll keys

--- keymap.c
+++ keymap.c
@@ -164,9 +164,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     } else if (index == 1) {
         // Page up/Page down
         if (clockwise) {
-            tap_code(KC_LEFT);
-        } else {
             tap_code(KC_RGHT);
+        } else {
+            tap_code(KC_LEFT);
         }
     }
     return false;
