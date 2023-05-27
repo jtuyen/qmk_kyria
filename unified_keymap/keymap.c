c7657cdde36e2b3b24ef5d8231b42efeaf9c743c
John Tuyen <john.tuyen@gmail.com>
2023-05-27 13:29:09 -0400

    switching to ffwd and rwd

--- unified_keymap/keymap.c
+++ unified_keymap/keymap.c
@@ -187,11 +187,10 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_VOLD);
         }
     } else if (index == 1) {
-        // Page up/Page down
         if (clockwise) {
-            tap_code(KC_DOWN);
+            tap_code16(LSG(KC_RIGHT));
         } else {
-            tap_code(KC_UP);
+            tap_code16(LSG(KC_LEFT));
         }
     }
     return false;
